18011371d1821f148bf0500c46679b11bcf02b13
dean <deanvelopermn@gmail.com>
2024-08-17 10:42:52 -0700

    how did i forget to add root source file

--- build.zig
+++ build.zig
@@ -8,20 +8,20 @@ pub fn build(b: *std.Build) void {
     const extras_module = extras.module("extras");
 
     const time_module = b.addModule("time", .{
+        .root_source_file = b.path("time.zig"),
         .optimize = optimize,
         .target = target,
         .imports = &.{.{ .name = "extras", .module = extras_module }},
     });
 
+    // zig build test
+    const t_step = b.step("test", "Run all the tests.");
     const t = b.addTest(.{
         .optimize = optimize,
         .target = target,
         .root_source_file = b.path("main.zig"),
     });
     t.root_module.addImport("time", time_module);
-
     const run_t = b.addRunArtifact(t);
-
-    const t_step = b.step("test", "Run all the tests.");
     t_step.dependOn(&run_t.step);
 }
